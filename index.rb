6308803341ae21ed7ab979f6d6b2e695df7539b1
Noah Davis <arbreperdu@gmail.com>
2016-10-17 15:22:17 -0700

    Example of using a case/when instead of an if/else

--- index.rb
+++ index.rb
@@ -5,18 +5,19 @@ Dir.glob('./lib/**/*.rb').each { |f| require f }
 data = (file = ARGV.first) ? File.open(file) : STDIN.read.split("\n")
 
 data.each_line do |commands|
-  inputs = commands.split
-  record_type = inputs.shift.downcase
+  inputs       = commands.split
+  record_type  = inputs.shift.downcase
   record_owner = inputs.shift
 
-  start = record_type == 'trip' ? Time.parse(inputs.shift) : nil
-  stop = record_type == 'trip' ? Time.parse(inputs.shift) : nil
-  distance = record_type == 'trip' ? inputs.shift.to_f : nil
-  duration = record_type == 'trip' ? (stop - start) / 3600 : nil
+  start    = record_type == 'trip' ? Time.parse(inputs.shift) : nil
+  stop     = record_type == 'trip' ? Time.parse(inputs.shift) : nil
+  distance = record_type == 'trip' ? inputs.shift.to_f        : nil
+  duration = record_type == 'trip' ? (stop - start) / 3600    : nil
 
-  if record_type == 'trip'
+  case record_type
+  when 'trip'
     Trip.new(record_owner, distance, duration)
-  elsif record_type == 'driver'
+  when 'driver'
     Driver.new(record_owner)
   else
     puts 'Invalid data entry. Please check data for errors.'
